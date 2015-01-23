07a8346ac2fed7bd0cd8807d8977f955c59b38fd
Nils Grunwald <nils@grunwald.fr>
2015-01-23 12:25:16 +0100

    update deps, bump version and add travis

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.oscaro/fleming "0.1.0-SNAPSHOT"
+(defproject com.oscaro/fleming "0.1.0"
   :description "Small library for browser user agent identification, based on the UADetector Java library"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/oscaro/fleming"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.sf.uadetector/uadetector-resources "2014.10"]
                  [gavagai "0.3.2"]
-                 [org.clojure/core.memoize "0.5.6"]]
-  :profiles {:dev {:dependencies [[expectations "2.0.9"]]
+                 [org.clojure/core.memoize "0.5.7"]]
+  :profiles {:dev {:dependencies [[expectations "2.0.13"]]
                    :plugins [[lein-expectations "0.0.7"]]}})
